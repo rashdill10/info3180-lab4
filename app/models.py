a85c7ffdcf3837d3780813fad7fa3d331adf1432
Rashene Dillon <rashwork2024@gmail.com>
2026-03-16 16:50:30 -0500

    Exercise 1, 2 and 3 - migrations and user model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from . import db
-
+from werkzeug.security import generate_password_hash
 
 class UserProfile(db.Model):
     # You can use this to change the table name. The default convention is to use
@@ -12,7 +12,14 @@ class UserProfile(db.Model):
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(128), nullable=False)
 
+    def __init__(self, first_name, last_name, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = generate_password_hash(password, method='pbkdf2:sha256')
+  
     def is_authenticated(self):
         return True
 
@@ -23,10 +30,7 @@ class UserProfile(db.Model):
         return False
 
     def get_id(self):
-        try:
-            return unicode(self.id)  # python 2 support
-        except NameError:
-            return str(self.id)  # python 3 support
+        return str(self.id)  # python 3 support
 
     def __repr__(self):
         return '<User %r>' % (self.username)
