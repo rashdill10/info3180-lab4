37260ae9461c8500a67f3ec142a17927be178804
Rashene Dillon <rashwork2024@gmail.com>
2026-03-16 17:06:14 -0500

    Exercise 4-login route

--- app/views.py
+++ app/views.py
@@ -41,26 +41,23 @@ def upload():
 def login():
     form = LoginForm()
 
-    # change this to actually validate the entire form submission
-    # and not just one field
-    if form.username.data:
-        # Get the username and password values from the form.
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
 
-        # Using your model, query database for a user based on the username
-        # and password submitted. Remember you need to compare the password hash.
-        # You will need to import the appropriate function to do so.
-        # Then store the result of that query to a `user` variable so it can be
-        # passed to the login_user() method below.
+        from werkzeug.security import check_password_hash
+        user = db.session.execute(db.select(UserProfile).filter_by(username=username)).scalar()
 
-        # Gets user id, load into session
-        login_user(user)
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('upload'))
+        else:
+            flash('Invalid username or password.', 'danger')
 
-        # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
     return render_template("login.html", form=form)
-
-# user_loader callback. This callback is used to reload the user object from
-# the user ID stored in the session
+    # user_loader callback. This callback is used to reload the user object from
+    # the user ID stored in the session
 @login_manager.user_loader
 def load_user(id):
     return db.session.execute(db.select(UserProfile).filter_by(id=id)).scalar()
